a2651f2dba5f7c39803bc8bd29393606df816ac9
Simon Tucker <sjtucker28@gmail.com>
2019-12-11 20:10:51 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,16 +4,14 @@ end
 
 def subtraction(num1, num2)
   subtraction = num1 - num2
-  subtraction
 end
 
 def division(num1, num2)
   division = num1/num2
-  division
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
 end
 
 def modulo(num1, num2)
