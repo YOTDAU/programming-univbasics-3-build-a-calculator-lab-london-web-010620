1652978db28e26442b8a5cab14978ed85d0a441a
Simon Tucker <sjtucker28@gmail.com>
2019-12-11 20:10:20 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,14 +1,15 @@
 def addition(num1, num2)
   addition = num1 + num2
-  addition
 end
 
 def subtraction(num1, num2)
-
+  subtraction = num1 - num2
+  subtraction
 end
 
 def division(num1, num2)
-
+  division = num1/num2
+  division
 end
 
 def multiplication(num1, num2)
